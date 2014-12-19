56b225108a8814b0b53417b2f922484f711030e5
Mazdak Momen <mazdak.momen@gmail.com>
2014-12-19 10:51:02 -0800

    Adding comments for string_index for future reference

--- string_index.rb
+++ string_index.rb
@@ -1,10 +1,10 @@
 def string_index(num)
-  if num[-1].to_i + 1 == 10
-    num = string_index(num[0...num.length-1]) + '0'
-  elsif num == ''
+  if num[-1].to_i + 1 == 10 #if incrementing last digit gives 10
+    num = string_index(num[0...num.length-1]) + '0' #change num to equal the string_index of the number excluding the last digit + 0 as a string
+  elsif num == '' # if num is empty, because original is all 9s num will become 1
     num = '1'
   else
-    num[-1] = (num[-1].to_i + 1).to_s
+    num[-1] = (num[-1].to_i + 1).to_s #here simply increment the last character by one and convert to string
   end
   return num
 end
